5c26220b4a81984de490f8c2d61144db51f5c95d
Mark Kastner <mark.kastner@outlook.com>
2016-08-22 17:47:38 +0200

    Berechtigungen in Home

--- Placez/app/src/main/java/wusadevelopment/albert/com/placez/Home.java
+++ Placez/app/src/main/java/wusadevelopment/albert/com/placez/Home.java
@@ -1,9 +1,13 @@
 package wusadevelopment.albert.com.placez;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -16,6 +20,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.util.List;
@@ -59,6 +64,16 @@ public class Home extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        int MyVersion = Build.VERSION.SDK_INT;
+        if (MyVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
+            if (!checkIfAlreadyhaveLocationPermission()) {
+                requestForLocationPermission();
+            }
+            if (!checkIfAlreadyhaveStoragePermission()) {
+                requestForStoragePermission();
+            }
+        }
     }
 
     @Override
@@ -115,6 +130,54 @@ public class Home extends AppCompatActivity
         return true;
 
 
+    }
+
+    private boolean checkIfAlreadyhaveLocationPermission() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestForLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+    }
+
+    private boolean checkIfAlreadyhaveStoragePermission() {
+        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void requestForStoragePermission() {
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 102);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    //not granted
+                    Toast.makeText(this, R.string.locationWarning, Toast.LENGTH_LONG).show();
+                }
+                break;
+            case 102:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    //not granted
+                    Toast.makeText(this, R.string.storageWarning, Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
--- Placez/app/src/main/java/wusadevelopment/albert/com/placez/MapsActivity.java
+++ Placez/app/src/main/java/wusadevelopment/albert/com/placez/MapsActivity.java
@@ -23,8 +23,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public static double longitude = 0.0;
     public static double latitude = 0.0;
     public static Marker currentPositionMarker;
-    boolean locationpermissionGranted = false;
-    boolean storagepermissionGranted = false;
     private LocationService ls;
 
     @Override
@@ -32,83 +30,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        int MyVersion = Build.VERSION.SDK_INT;
-        if (MyVersion > Build.VERSION_CODES.LOLLIPOP_MR1) {
-            if (!checkIfAlreadyhaveLocationPermission()) {
-                requestForLocationPermission();
-            }else{
-                locationpermissionGranted = true;
-            }
-            if (!checkIfAlreadyhaveStoragePermission()) {
-                requestForStoragePermission();
-            }else{
-                storagepermissionGranted = true;
-            }
-        }
-        if(locationpermissionGranted && storagepermissionGranted) {
-            ls = new LocationService(this);
-            this.longitude = ls.getLongitude();
-            this.latitude = ls.getLatitude();
+        ls = new LocationService(this);
+        this.longitude = ls.getLongitude();
+        this.latitude = ls.getLatitude();
 
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-        }
-    }
-
-    private boolean checkIfAlreadyhaveLocationPermission() {
-        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void requestForLocationPermission() {
-        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
-    }
-
-    private boolean checkIfAlreadyhaveStoragePermission() {
-        int result = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        if (result == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    private void requestForStoragePermission() {
-        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 102);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case 101:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    //granted
-                    LocationService ls = new LocationService(this);
-                    this.longitude = ls.getLongitude();
-                    this.latitude = ls.getLatitude();
-                    locationpermissionGranted = true;
-                } else {
-                    //not granted
-                    Toast.makeText(this, R.string.locationWarning, Toast.LENGTH_LONG).show();
-                }
-                break;
-            case 102:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    storagepermissionGranted = true;
-                } else {
-                    //not granted
-                    Toast.makeText(this, R.string.storageWarning, Toast.LENGTH_LONG).show();
-                }
-                break;
-            default:
-                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
